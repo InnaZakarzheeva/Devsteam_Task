b748f1081fb3bc4d23552a4eaff8c0c3f7797863
InnaZakarzheieva <44522230+InnaZakarzheeva@users.noreply.github.com>
2019-10-08 21:24:32 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import {store} from './store/store';
-import {Provider} from 'react-redux';
 import ImageGallery from './component/gallery'
 
 export default class App extends React.Component{
   render(){
     return (
-      <Provider store={store}>
         <ImageGallery/>
-      </Provider>
     );
   }
 }
